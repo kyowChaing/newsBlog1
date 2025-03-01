1751baec93d5d71b3be0588d2475628899e38526
Kyow <kyowchaing15@gmail.com>
2025-03-01 22:57:00 +0600

    Add Social Media Link

--- src/components/RightAside.jsx
+++ src/components/RightAside.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
 import { CiLink } from "react-icons/ci";
+
 
 import qzoneImg1 from '../assets/swimming.png';
 import qzoneImg2 from '../assets/class.png';
@@ -32,15 +32,15 @@ const RightAside = () => {
         <div className=' p-2'>
             <h2 className=' font-bold mb-2'> Find Us On</h2>
             <div className=' border-2 rounded-t rounded-b'>
-                <a href='' className=' flex justify-start  items-center p-4'>
+                <a target=' ' href='https://www.facebook.com/kyow.chak.k/' className=' flex justify-start  items-center p-4'>
                     <FaFacebook className=' mr-3' />
                     <span className=' text-[#706f6f]'>Facebook </span>
                 </a>
-                <a href='' className=' flex justify-start border-t-2  border-b-2 items-center p-4'>
-                    <FaTwitter className=' mr-3' />
-                    <span className=' text-[#706f6f]'> Twitter </span>
+                <a target=' ' href='https://github.com/kyowChaing' className=' flex justify-start border-t-2  border-b-2 items-center p-4'>
+                <FaGithub  className=' mr-3'/>
+                <span className=' text-[#706f6f]'> GitHub </span>
                 </a>
-                <a href=" " className=' flex justify-start  items-center p-4'>
+                <a target=' ' href="https://www.linkedin.com/in/kyow/" className=' flex justify-start  items-center p-4'>
                     <CiLink className='mr-3' />
                     <span className=' text-[#706f6f]'>LinkInd </span>
                 </a>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,33 @@ import Header from '../components/Header'
 
 const About = () => {
   return (
-    <div>About
+    <div>
         <Header></Header>
+        <div> 
+          <h2> Top Contributor </h2>
+          <hr />
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>Who we are </p>
+            <h2></h2>
+            <p></p>
+          </div>
+        </div>
+        <div className=''>
+          <div className='flex bg-[#EFF0F3]'>
+            <img src="" alt="" />
+            <div>
+              <h2 className='font-black'>name</h2>
+              <p className='text-[#7D8894]'> position</p>
+              <div className='flex items-center'>
+                <span> publish icon</span>
+                <p className='text-[#7D8894]'>03 publishd post</p>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
